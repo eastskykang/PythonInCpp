260e02989a1f7384ffbdd26266ab6dcf0d3bfe97
donghokang <east0822@gmail.com>
2019-08-16 15:23:36 +0200

    initial commit

---
+++ tf-python.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import network
+import numpy as np
+import time
+
+batch_size = 32
+input_shape = 128
+
+num_samples = 100
+
+# mlp
+input = tf.placeholder(tf.float32, shape=(batch_size, input_shape))
+h = tf.tanh(network.linear_custom(input, 'fc1', n_hidden=128, histogram=True))
+h = tf.tanh(network.linear_custom(h, 'fc2', n_hidden=128, histogram=True))
+
+init_all_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init_all_op)
+
+    start = time.time()
+    for _ in range(num_samples):
+        out = sess.run([h], feed_dict={
+            input: np.random.rand(batch_size, input_shape)
+        })
+
+        # print(out)
+
+    end = time.time()
+    print(end - start)
